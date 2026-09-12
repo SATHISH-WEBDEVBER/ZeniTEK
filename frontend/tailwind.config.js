68beb2497323fb05cd5fa1ba84596c06760ad07d
SATHISH-WEBDEVBER <sathishms1589@gmail.com>
2026-09-12 09:00:01 +0530

    feat: add Tailwind CSS configuration and global styles

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -24,6 +24,18 @@ export default {
       },
       fontFamily: {
         sans: ['Inter', 'system-ui', 'sans-serif'],
+      },
+      fontSize: {
+        '2xs': ['0.8125rem', { lineHeight: '1.25rem' }],
+        'xs': ['0.9375rem', { lineHeight: '1.45rem' }],   // ~15px-16px (previously 12px)
+        'sm': ['1.03125rem', { lineHeight: '1.55rem' }],  // ~16.5px-17.5px (previously 14px)
+        'base': ['1.125rem', { lineHeight: '1.75rem' }],  // ~18px-19px (previously 16px)
+        'lg': ['1.25rem', { lineHeight: '1.85rem' }],     // ~20px-21px
+        'xl': ['1.4375rem', { lineHeight: '2rem' }],      // ~23px-24px
+        '2xl': ['1.75rem', { lineHeight: '2.25rem' }],    // ~28px-30px
+        '3xl': ['2.25rem', { lineHeight: '2.65rem' }],    // ~36px-38px
+        '4xl': ['2.875rem', { lineHeight: '1.15' }],      // ~46px-48px
+        '5xl': ['3.5rem', { lineHeight: '1.1' }],         // ~56px-60px
       }
     },
   },
